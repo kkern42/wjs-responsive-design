0d448e8af62fadc453eeb624fdf2637e06728f89
Kevin Kern <kjk9cd@virginia.edu>
2020-06-01 16:46:09 -0400

    api button logs to console thats good

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import './App.css';
 import logo from './The-Wall-Street-Journal-Logo.png'
 //hey
 function App() {
+
+  const onChange = () => {
+    fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        "x-rapidapi-key": "fb96d153d3msha8bb9a91c05b002p1291b0jsncb0b53d50014"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <div>
       <header className="header">
         <div className='wrapper'>
-          <img src={logo} alt="Logo" class="center" />
+          <img src={logo} alt="Logo" className="center" />
         </div>
       </header>
+      <button onClick={onChange}>Access API</button>
     </div>
   );
 }
